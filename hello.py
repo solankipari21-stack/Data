b08b3f7d55036b8e1d5e0e5f84e4202aeb3096bd
pari solanki <solankipari21@gmail.com>
2026-09-13 15:23:02 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ print("hello kitty")
 print("I am learning Git and GitHub.")
 print("this is vs code")
 print("i am studying data analyst course")
-print("this id hit hub platform")
+print("this is git hub platform")
 
